bf5619b66ba2b88c2349f33f68416724494f601f
sh1nine96 <sh1nine96@gmail.com>
2026-07-16 15:03:58 +0530

    detect objects in multiple images

--- main.py
+++ main.py
@@ -11,27 +11,68 @@ paths = get_project_paths()
 model = load_model()  # load a pretrained YOLOv8n model
 
 #input image path
-image_path = paths["images"] / "street.jpg"
+image_paths = list(paths["images"].glob("*.jpg"))  # get all jpg images in the images folder
+print(f'Found {len(image_paths)} images in the images folder.')
+# print(f'Using image: {image_paths[0]}')  # use the first image for testing
+# for i in image_paths:
+#     print(f'Using image: {i}')  # use the first image for testing
 
-# read image
-image = cv2.imread(str(image_path))
-
-
-# perform detection
-results = predict_image(model, image_path, conf=0.5)
-
-# draw detections on the image
-
-annotated_image = draw_predictions(image, results[0], model)
+for image_path in image_paths:
+    print(f'Using image: {image_path.name}')  # use the first image for testing
 
 
-# Convert BGR to RGB for displaying with matplotlib
-annotated_image_rgb = cv2.cvtColor(annotated_image, cv2.COLOR_BGR2RGB)
+
+for image_path in image_paths:
+    #read image
+    image = cv2.imread(str(image_path))
+
+    #Detect objects in the image
+    results = predict_image(
+        model,
+        image_path,
+        conf=0.5
+    )
+
+    # draw detections on the image
+    annotated_image = draw_predictions(
+        image, 
+        results[0], 
+        model
+        )
+    
+
+    # Convert BGR to RGB for displaying with matplotlib
+    annotated_image = cv2.cvtColor(
+        annotated_image, 
+        cv2.COLOR_BGR2RGB
+    )
+
+    #Display the annotated image
+    plt.figure(figsize=(12, 8))
+    plt.imshow(annotated_image)
+    plt.title(f"Detected Objects in {image_path.name}")
+    plt.axis('off')
+    plt.show()
+
+# # read image
+# image = cv2.imread(str(image_path))
 
 
-# display the annotated image
-plt.figure(figsize=(12, 8))
-plt.imshow(annotated_image_rgb)
-plt.title("Detected Objects")
-plt.axis('off')
-plt.show()
+# # perform detection
+# results = predict_image(model, image_path, conf=0.5)
+
+# # draw detections on the image
+
+# annotated_image = draw_predictions(image, results[0], model)
+
+
+# # Convert BGR to RGB for displaying with matplotlib
+# annotated_image_rgb = cv2.cvtColor(annotated_image, cv2.COLOR_BGR2RGB)
+
+
+# # display the annotated image
+# plt.figure(figsize=(12, 8))
+# plt.imshow(annotated_image_rgb)
+# plt.title("Detected Objects")
+# plt.axis('off')
+# plt.show()
